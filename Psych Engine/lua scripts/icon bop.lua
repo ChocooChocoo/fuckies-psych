7c0ffee2d8529332d1e32cb8ecd79ae3a4c82312
Dayo <125631349+ilovedayo@users.noreply.github.com>
2023-04-23 02:44:56 -0500

    Update icon bop.lua

--- Psych Engine/lua scripts/icon bop.lua
+++ Psych Engine/lua scripts/icon bop.lua
@@ -1,4 +1,4 @@
-local icon =  "Default" --// Default, Scale Verticle, Scale Horizontal, Scale Diangle
+local icon =  "Default" --// Default, Scale Verticle, Default V2
 
 function onCreate()
     makeLuaSprite('iconP1obj', 'iconobj', 0, 0)
@@ -21,63 +21,29 @@ function onBeatHit()
         if dance then
             scaleObject('iconP1obj', 1, 0.4)
             scaleObject('iconP2obj', 1, 1.5)
-            setProperty('iconP1.y', getProperty('iconP1.y') - 40)
-            setProperty('iconP2.y', getProperty('iconP2.y') + 40)
-            doTweenY('icon1', 'iconP1', getProperty('iconP1.y') + 40, crochet / 1300, 'circOut')
-            doTweenY('icon2', 'iconP2', getProperty('iconP2.y') - 40, crochet / 1300, 'circOut')
+            setProperty('iconP1.y', getProperty('iconP1.y') - 20)
+            setProperty('iconP2.y', getProperty('iconP2.y') + 20)
+            doTweenY('icon1', 'iconP1', getProperty('iconP1.y') + 20, crochet / 1000, 'circOut')
+            doTweenY('icon2', 'iconP2', getProperty('iconP2.y') - 20, crochet / 1000, 'circOut')
         else
             scaleObject('iconP1obj', 1, 1.5)
             scaleObject('iconP2obj', 1, 0.4)
-            setProperty('iconP1.y', getProperty('iconP1.y') + 40)
-            setProperty('iconP2.y', getProperty('iconP2.y') - 40)
-            doTweenY('icon11', 'iconP1', getProperty('iconP1.y') - 40, crochet / 1300, 'circOut')
-            doTweenY('icon22', 'iconP2', getProperty('iconP2.y') + 40, crochet / 1300, 'circOut')
+            setProperty('iconP1.y', getProperty('iconP1.y') + 20)
+            setProperty('iconP2.y', getProperty('iconP2.y') - 20)
+            doTweenY('icon1', 'iconP1', getProperty('iconP1.y') - 20, crochet / 1000, 'circOut')
+            doTweenY('icon2', 'iconP2', getProperty('iconP2.y') + 20, crochet / 1000, 'circOut')
         end
-        doTweenY('iconScale1', 'iconP1obj.scale', 1, crochet / 1300, 'circOut')
-        doTweenY('iconScale2', 'iconP2obj.scale', 1, crochet / 1300, 'circOut')
+        doTweenY('iconScale1', 'iconP1obj.scale', 1, crochet / 1000, 'circOut')
+        doTweenY('iconScale2', 'iconP2obj.scale', 1, crochet / 1000, 'circOut')
     end
-    if icon == "Scale Horizontal" then
-        if dance then
-            scaleObject('iconP1obj', 0.4, 1)
-            scaleObject('iconP2obj', 1.5, 1)
-            setProperty('iconP1.y', getProperty('iconP1.x') - 40)
-            setProperty('iconP2.y', getProperty('iconP2.x') + 40)
-            doTweenX('icon12', 'iconP1', getProperty('iconP1.x') + 40, crochet / 1300, 'circOut')
-            doTweenX('icon21', 'iconP2', getProperty('iconP2.x') - 40, crochet / 1300, 'circOut')
-        else
-            scaleObject('iconP1obj', 1.5, 1)
-            scaleObject('iconP2obj', 0.4, 1)
-            setProperty('iconP1.x', getProperty('iconP1.x') + 40)
-            setProperty('iconP2.x', getProperty('iconP2.x') - 40)
-            doTweenX('icon14', 'iconP1', getProperty('iconP1.x') - 40, crochet / 1300, 'circOut')
-            doTweenX('icon24', 'iconP2', getProperty('iconP2.x') + 40, crochet / 1300, 'circOut')
+    if icon == "Default V2" then
+        local turn = 20 
+        if curBeat % 2 == 0 then
+            turn = -20
         end
-        doTweenX('iconScale1', 'iconP1obj.scale', 1, crochet / 1300, 'circOut')
-        doTweenX('iconScale2', 'iconP2obj.scale', 1, crochet / 1300, 'circOut')
-    end
-    if icon == "Scale Diangle" then
-        if dance then
-            scaleObject('iconP1obj', 1, 0.2)
-            scaleObject('iconP2obj', 1, 1.9)
-            setProperty('iconP1.y', getProperty('iconP1.y') - 40)
-            setProperty('iconP2.y', getProperty('iconP2.y') + 40)
-            setProperty('iconP1.angle', 50)
-            setProperty('iconP2.angle', -50)
-            doTweenY('icon15', 'iconP1', getProperty('iconP1.y') + 40, crochet / 1300, 'circOut')
-            doTweenY('icon26', 'iconP2', getProperty('iconP2.y') - 40, crochet / 1300, 'circOut')
-        else
-            scaleObject('iconP1obj', 1, 1.9)
-            scaleObject('iconP2obj', 1, 0.2)
-            setProperty('iconP1.angle', -50)
-            setProperty('iconP2.angle', 50)
-            setProperty('iconP1.y', getProperty('iconP1.y') + 40)
-            setProperty('iconP2.y', getProperty('iconP2.y') - 40)
-            doTweenY('icon13', 'iconP1', getProperty('iconP1.y') - 40, crochet / 1300, 'circOut')
-            doTweenY('icon27', 'iconP2', getProperty('iconP2.y') + 40, crochet / 1300, 'circOut')
-        end
-        doTweenY('iconScale1', 'iconP1obj.scale', 1, crochet / 1300, 'circOut')
-        doTweenY('iconScale2', 'iconP2obj.scale', 1, crochet / 1300, 'circOut')
-        doTweenAngle('iconTween1','iconP1', 0, crochet / 1300,'circOut')
-        doTweenAngle('iconTween2','iconP2', 0, crochet / 1300,'circOut')
+        setProperty('iconP1.angle', turn)
+        setProperty('iconP2.angle', -turn)
+        doTweenAngle('icon1e', 'iconP1', 0, crochet / 1000, 'circOut')
+        doTweenAngle('icon2e', 'iconP2', 0, crochet / 1000, 'circOut')
     end
 end
